5dd25286b74dc8be8684b0f169d9c654c15bf77c
toni-mass <antoniomassidda6@gmail.com>
2025-08-16 14:21:40 +0200

    Simplified comments and documentations

--- easy/divideAStringIntoGroupsOfK.py
+++ easy/divideAStringIntoGroupsOfK.py
@@ -1,31 +1,13 @@
-"""
-Title: Divide a String Into Groups of Size k
-Difficulty: Easy
-Language: Python
-
-Description:
-    You are given a string 's' that can be split into groups of size 'k'.
-    If the last group is made of less than 'k' elements, the remaining is filled with the 'fill' character.
-    Return a list of all the groups.
-
-Example:
-    Input: s = "cbaedfihg", k = 3, fill = "x"
-    Output: ["cba","edf","ihg"]
-
-Constraints:
-    - 1 <= len(s) <= 100
-    - 's' is made of only lowercase English letters.
-    - 1 <= k <= 100
-    - 'k' can only be a lowercase English letter.
-
-Notes:
-    Inspired by LeetCode problem #2138 (Divide a String Into Groups of Size k).
-"""
+"""Inspired by LeetCode problem #2138 (Divide a String Into Groups of Size k)."""
 
 from typing import List
 
 def divideString(s: str, k: int, fill: str) -> List[str]:
     """
+    You are given a string `s` that can be split into groups of size `k`.
+    If the last group is made of less than `k` elements, the remaining is filled with the `fill` character.
+    Return a list of all the groups.
+
     Args:
         s (str):    string of lowercase English characters.
         k (int):    number of elements per group.
@@ -36,14 +18,14 @@ def divideString(s: str, k: int, fill: str) -> List[str]:
     """
 
     ans = []
-    for i in range(0, len(s), k):                   #] Append all the groups to the answer.
-        ans.append(s[i:i+k])                        #]
-    ans[-1] = ans[-1] + fill * (k - len(ans[-1]))   #} Fill in the last eventual incomplete group
-                                                    #} with the 'fill' character.
+
+    # Append all the groups to ans.
+    for i in range(0, len(s), k):
+        ans.append(s[i:i+k])
+    
+    # Fill in the last eventual incomplete group with the 'fill' character.
+    ans[-1] = ans[-1] + fill * (k - len(ans[-1]))
     return ans
 
 if __name__ == "__main__":
-    s = "cbaedfihg"
-    k = 3
-    fill = "x"
-    print(divideString(s, k, fill))
+    print(divideString("cbaedfihg", 3, "x"))
--- easy/missingNumber.py
+++ easy/missingNumber.py
@@ -1,29 +1,12 @@
-"""
-Title: Missing Number
-Difficulty: Easy
-Language: Python
-
-Description:
-    You are given an array of length n and containing all the numbers in the range [0, n],
-    except for one. Find that missing number and return it.
-
-Example:
-    Input: nums = [0, 3, 1]
-    Output: 2
-
-Constraints:
-    - n == len(nums)
-    - 1 <= n <= 10^4
-    - every nums[i] is unique and in the range [0, n].
-
-Notes:
-    Inspired by LeetCode problem #268 (Missing Number).
-"""
+"""Inspired by LeetCode problem #268 (Missing Number)."""
 
 from typing import List
 
 def missingNumber(nums : List[int]) -> int:
     """
+    You are given an array of length n and containing all the numbers in the range [0, n], except for one.
+    Find that missing number and return it.
+
     Args:
         nums (List[int]):   List of n distinct integers in the range [0, n].
 
@@ -31,11 +14,15 @@ def missingNumber(nums : List[int]) -> int:
         int:    The missing integer in the range.
     """
 
-    ans = (len(nums) * (len(nums)+1)) // 2      # Sum all the numbers in the range [0, n].
-    for i in nums:                              #] Remove each element of nums from the sum.
-        ans -= i                                #]
-    return ans                                  # Return the remaining number.
+    # Sum all the numbers in the range [0, n].
+    ans = (len(nums) * (len(nums)+1)) // 2
+
+    # Remove each element of nums from the sum.
+    for i in nums:
+        ans -= i
+
+    # Return the remaining number
+    return ans
 
 if __name__ == "__main__":
-    example = [0, 3, 1]
-    print(missingNumber(example))
+    print(missingNumber([0, 3, 1]))
