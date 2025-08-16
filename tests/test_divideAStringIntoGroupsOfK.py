6351a82bc1462d6d27c8ccbc6065468f448dbf62
toni-mass <antoniomassidda6@gmail.com>
2025-08-16 15:21:12 +0200

    Removed files with old names

--- tests/test_divideAStringIntoGroupsOfK.py
+++
@@ -1,5 +0,0 @@
-from easy.divideAStringIntoGroupsOfK import divideString
-
-def test_divideString():
-    assert divideString("cbaedfihg", 3, "x") == ['cba', 'edf', 'ihg']
-    assert divideString("cbaedfihgj", 3, "x") == ['cba', 'edf', 'ihg', 'jxx']
